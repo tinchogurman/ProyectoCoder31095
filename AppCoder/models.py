31605bbac99a1be106d2bbc031f0a74482a6687a
Martin Gurman <tincho.gurman@gmail.com>
2022-08-23 01:23:12 -0300

    Familia updated

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -7,21 +7,4 @@ class Familiares(models.Model):
     edad = models.IntegerField()
     fecha_de_nacimiento = models.DateField()
 
-class Estudiantes(models.Model):
 
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    email = models.EmailField()
-
-class Profesor(models.Model):
-
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    email = models.EmailField()
-    profesion = models.CharField(max_length=30)
-
-class Entregable(models.Model):
-
-    nombre = models.CharField(max_length=30)
-    fecha_de_entrega = models.DateField()
-    entregado = models.BooleanField()
