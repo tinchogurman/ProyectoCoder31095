937ba38d07313688463c032a3037c57f6e981e67
Martin Gurman <tincho.gurman@gmail.com>
2022-08-24 00:09:30 -0300

    changed names

--- Mi_MVT/views.py
+++ Mi_MVT/views.py
@@ -7,7 +7,7 @@ def familia(request):
     familia1 = Familiares(nombre="Federico", edad=36, fecha_de_nacimiento='1987-04-07')
     familia1.save()
     contexto = {
-        'familia': familia1
+        'familia0': familia1
     }
 
     return render(request, 'curso.html', contexto)
