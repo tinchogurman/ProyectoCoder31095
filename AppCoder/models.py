4ffa9514c3401c5f65a365abe7595a8c75e71a20
Martin Gurman <tincho.gurman@gmail.com>
2022-08-27 21:44:20 -0300

    Rollback

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class Familiares(models.Model):
+class Curso(models.Model):
 
     nombre = models.CharField(max_length=40)
-    edad = models.IntegerField()
-    fecha_de_nacimiento = models.DateField()
+    camada = models.IntegerField()
 
+class Estudiante(models.Model):
 
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+class Entregable(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    fechaDeEntrega = models.DateField()
+    entregado = models.BooleanField()
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 
-from AppCoder.models import Familiares
+from AppCoder.models import Curso
 
-def familia(request):
+def curso(request):
 
-    familia1 = Familiares(nombre="Federico Gurman", edad=36, fecha_de_nacimiento='1987-04-07')
-    familia1.save()
-    familia2 = Familiares(nombre="Evelyn Gurman", edad=38, fecha_de_nacimiento='1984-01-11')
-    familia2.save()
-    familia3 = Familiares(nombre="Luis Gurman", edad=45, fecha_de_nacimiento='1977-10-05')
-    familia3.save()
+    curso1 = Curso(nombre="Martin Gurman", camada=36)
+    curso1.save()
+
     contexto = {
-        'familia': familia1,
-        'familia2': familia2,
-        'familia3': familia3
+        'curso': curso1
+
     }
 
     return render(request, 'curso.html', contexto)
