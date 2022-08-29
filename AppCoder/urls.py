d053272bae27bc75471195d6888220dd1a5acfe5
Martin Gurman <tincho.gurman@gmail.com>
2022-08-28 23:19:00 -0300

    Aplicada la plantilla

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from AppCoder.views import curso
+from AppCoder.views import curso, inicio
 
 urlpatterns = [
+    path('', inicio),
     path('curso/', curso)
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from AppCoder.models import Curso
 
+def inicio(request):
+    return render(request, 'index.html')
+
 def curso(request):
 
     curso1 = Curso(nombre="Martin Gurman", camada=36)
