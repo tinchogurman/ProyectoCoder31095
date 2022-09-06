60d71c72b99300dc283f086516f9d74829693194
Martin Gurman <tincho.gurman@gmail.com>
2022-09-05 21:21:43 -0300

    Last changes

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,19 +4,19 @@ from django.db import models
 class Curso(models.Model):
 
     nombre = models.CharField(max_length=40)
-    camada = models.IntegerField()
+    camada = models.IntegerField(unique=True)
 
 class Estudiante(models.Model):
 
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
 class Profesor(models.Model):
 
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     profesion = models.CharField(max_length=30)
 
 class Entregable(models.Model):
